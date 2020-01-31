cdbdf8b472bde4692bdeded0aadc47253bab9945
Gabriel28 <nasc.gabriel.1997@gmail.com>
2020-01-31 18:16:22 -0300

    fix error

--- controle-games/routes/web.php
+++ controle-games/routes/web.php
@@ -10,16 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
 Route::get('/games', 'GamesController@index')
     ->name('listar_games');
 Route::get('/games/criar', 'GamesController@create')
     ->name('form_criar_serie');
 Route::post('/games/criar', 'GamesController@store');
 Route::delete('/games/{id}', 'GamesController@destroy');
-=======
-
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> 4d89d62429711cdd12568f3fdcd1b90729a41781
